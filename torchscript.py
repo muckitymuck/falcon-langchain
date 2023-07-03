145b8fc8a3434bea22bce4d038ed5c5a9e488867
muckitymuck <andrewgoldstein@hotmail.com>
2023-07-03 14:21:08 -0500

    model_name

--- torchscript.py
+++ torchscript.py
@@ -2,9 +2,9 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # Replace 'model_name' with the name of the Hugging Face model you want to use
-model_name = 'model_name'
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model_name = 'tiiuae/falcon-7b'
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
 
 # Save the model in TorchScript format
 torch.jit.save(torch.jit.script(model), "torchscript_model.pt")
