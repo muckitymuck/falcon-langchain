e4536ad65622425b3eb23f94f1c8b0bfabe195b8
muckitymuck <andrewgoldstein@hotmail.com>
2023-07-08 18:04:30 -0500

    falconlocal.py

---
+++ falconlocal.py
@@ -0,0 +1,17 @@
+from flask import jsonify
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+@app.route('/falcon', methods=['POST'])
+def falcon():
+    text = request.json['text']
+    model_name = 'path/to/your/model'  # Replace with the path to your LLM model directory
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    input_ids = tokenizer.encode(text, return_tensors="pt")
+    generated_ids = model.generate(input_ids, max_length=200, num_return_sequences=1)
+    generated_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+
+    response = jsonify({'generated_text': generated_text})
+    return response
