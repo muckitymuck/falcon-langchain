af101aed3dcce9857868e9f9b3a197342d328273
muckitymuck <andrewgoldstein@hotmail.com>
2023-07-09 00:49:35 -0500

    remote code true

--- falconlc.py
+++ falconlc.py
@@ -3,10 +3,11 @@ from flask import Flask, jsonify, request
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 app = Flask(__name__)
+
 model_name = '/home/ubuntu/falcon-7b-instruct/'  # Replace with the path to your LLM model directory
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
 @app.route('/falcon', methods=['POST'])
 def falcon():
     text = request.json['text']
