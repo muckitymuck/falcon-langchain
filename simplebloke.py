43a2ff3c74001b6866a66eb7494a8641b3ea4611
muckitymuck <andrewgoldstein@hotmail.com>
2023-07-09 19:57:53 -0500

    simplebloke.py

---
+++ simplebloke.py
@@ -0,0 +1,53 @@
+from transformers import AutoTokenizer, pipeline, logging
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+import argparse
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+model_name_or_path = "TheBloke/falcon-7b-instruct-GPTQ"
+# You could also download the model locally, and access it there
+# model_name_or_path = "/path/to/TheBloke_falcon-7b-instruct-GPTQ"
+
+model_basename = "gptq_model-4bit-64g"
+
+use_triton = False
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
+        model_basename=model_basename,
+        use_safetensors=True,
+        trust_remote_code=True,
+        device="cuda:0",
+        use_triton=True,
+        quantize_config=None)
+
+prompt = "Tell me about AI"
+prompt_template=f'''A helpful assistant who helps the user with any questions asked.
+User: {prompt}
+Assistant:'''
+
+print("\n\n*** Generate:")
+
+input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
+output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
+print(tokenizer.decode(output[0]))
+
+# Inference can also be done using transformers' pipeline
+# Note that if you use pipeline, you will see a spurious error message saying the model type is not supported
+# This can be ignored!  Or you can hide it with the following logging line:
+# Prevent printing spurious transformers error when using pipeline with AutoGPTQ
+logging.set_verbosity(logging.CRITICAL)
+
+print("*** Pipeline:")
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=512,
+    temperature=0.7,
+    top_p=0.95,
+    repetition_penalty=1.15
+)
+
+print(pipe(prompt_template)[0]['generated_text'])
